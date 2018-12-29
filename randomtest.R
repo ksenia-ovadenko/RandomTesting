71f1d198d976cb8c1f08df11476a9e3cc7f2cfd7
ksenia-ovadenko <32774886+ksenia-ovadenko@users.noreply.github.com>
2018-12-29 16:37:35 +0100

    Update randomtest.R

--- randomtest.R
+++ randomtest.R
@@ -1,13 +1,13 @@
 library("RDieHarder")
 
 runtest = function (rng, test){
-  dht = dieharder(rng=rng, test=test, psamples=10000, seed=0, verbose=FALSE)
-  v = as.double(dht["p.value"])
+  dht = dieharder(rng=rng, test=test, psamples=10000, seed=0, verbose=FALSE)  ## repeat the test 10000 times, take a random seed
+  v = as.double(dht["p.value"])    ## see resulting p-value
   v
 }
 
-tests = c(0,1,2,5,10)
-results = sapply(tests, function(t) runtest(14, t))
-df = data.frame(tests, results)
+tests = c(0,1,2,5,10)    ## put the id numbers of tests we want to apply to the vector 
+results = sapply(tests, function(t) runtest(14, t)) ## repeating the function for PRNG #14 (Mersenne Twister) and chosen tests
+df = data.frame(tests, results) ## saving results to a data frame
 df
-min(df[,2])
+min(df[,2])  ## print the minimum p-value
