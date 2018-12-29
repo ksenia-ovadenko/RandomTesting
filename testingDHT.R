2f1a833ef4e66b5b17e8af25a6a5cea26260efa0
ksenia-ovadenko <32774886+ksenia-ovadenko@users.noreply.github.com>
2018-12-29 16:41:35 +0100

    Update testingDHT.R

--- testingDHT.R
+++ testingDHT.R
@@ -5,5 +5,5 @@ dieharderTest() ## gives the list of avaiable tests
 dht = dieharder(rng="Mersenne-Twister", test="diehard_birthdays", 
                 psamples=1000, seed=10, verbose=FALSE) ## we are choosing a RNG; a test; 
                                                       ##how many times we want to replicate it and 
-                                                      ##if we need detalied summary
+                                                      ##if we need a detailed summary
 plot(dht) ## plotting the results
